fa0672706233a129ac4e774917e45b1c353e4a68
Sheba1990 <57401985+Sheba1990@users.noreply.github.com>
2019-11-06 21:20:58 +0300

    getter & setter

--- src/com/it/academy/nikita/Cat.java
+++ src/com/it/academy/nikita/Cat.java
@@ -4,6 +4,17 @@ public class Cat {
 	static void Hi() {
 		System.out.println("Hi");
 	}
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public static void main(String[] args) {
 		Hi();
 	}
